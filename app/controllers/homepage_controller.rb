5f20d46b210cb39afe28df9c34cf2a9edc793609
Mickey Chen <poverwhelming@gmail.com>
2014-08-28 17:03:39 +0800

    fix flash message

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,11 +1,8 @@
-class HomepageController < ActionController::Base
+class HomepageController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def index
-  end
-
-  def sample
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def flash_class(level)
     case level
-      when :success then "ui green message"
-      when :error then "ui red message"
-      when :notice then "ui blue message"
+      when 'success' then "green"
+      when 'error' then "red"
+      when 'notice' then "yellow"
     end
   end
 end
