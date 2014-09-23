b39f2aff8516a156ce2088ae90f156c39464eacc
Mickey Chen <poverwhelming@gmail.com>
2014-09-23 23:05:07 +0800

    daily commit, HOOT

--- crawler/crawl_ai5429.rb
+++ crawler/crawl_ai5429.rb
@@ -8,7 +8,7 @@ require 'yaml'
 
 base_url = "http://www.ai5429.com/17/8x/"
 
-for i in 2..2
+for i in 1..1
   page_url = "#{"%05d"%i}.htm"
 
   url ="#{base_url}#{page_url}"
@@ -17,7 +17,7 @@ for i in 2..2
 
   gua_section = page.css("p.MsoNormal[align='left']")
 
-  p gua_section.text.split(/[\\n[[:space:]]]+/)
+  p gua_section.text.split(/[\\n[[:space:]]]+/).reject! { |c| c.empty? }
 
   sleep 1
 end
