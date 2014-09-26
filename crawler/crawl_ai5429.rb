073f6ba647c10be3ec5f20b0dbe7afd52efd0e5d
Mickey Chen <poverwhelming@gmail.com>
2014-09-26 21:52:55 +0800

    Revert "THIS IS GETTING ANNOYING, Im just gonna type them out, F**K Crawler"
    This reverts commit b8a21ea538377b5756ea95a276ef9eca1df5870f.

--- crawler/crawl_ai5429.rb
+++ crawler/crawl_ai5429.rb
@@ -8,11 +8,7 @@ require 'yaml'
 
 base_url = "http://www.ai5429.com/17/8x/"
 
-def strip_colon(text)
-  p "ohei!"
-end
-
-for i in 2..2
+for i in 1..1
   page_url = "#{"%05d"%i}.htm"
 
   url ="#{base_url}#{page_url}"
@@ -21,24 +17,7 @@ for i in 2..2
 
   gua_section = page.css("p.MsoNormal[align='left']")
 
-  yaos, xiangs = []
-
-  tuan_text = "彖曰"
-
-  xiang_text = "象曰"
-
-  yao_text = ["初九", "初六", "九一", "六一",
-              "九二", "六二", "九三", "六三",
-              "九四", "六四", "九五", "六五",
-              "上九", "上六"]
-
-  yung_text = ["用九", "用六"]
-
-  flag_text = /[#{[tuan_text, xiang_text, yao_text, yung_text].flatten.join("|")}]/
-
-  gua_array = gua_section.text.split(/[\\n[[:space:]]]+/).reject! { |c| c.empty? }
-
-  binding.pry
+  p gua_section.text.split(/[\\n[[:space:]]]+/).reject! { |c| c.empty? }
 
   sleep 1
 end
